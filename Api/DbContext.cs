b222bfc2376912ac41024b0e0685c5bcbbbd308f
zabronax <zabronax@gmail.com>
2025-04-01 11:21:40 +0200

    remove broken example

--- Api/DbContext.cs
+++
@@ -1,8 +0,0 @@
-
-using Microsoft.EntityFrameworkCore;
-
-public class BloggingContext(DbContextOptions<BloggingContext> options) : DbContext(options)
-{
-  public DbSet<Models.Blog> Blogs { get; set; }
-  public DbSet<Models.Post> Posts { get; set; }
-}
