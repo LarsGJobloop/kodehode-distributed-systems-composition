a24a6de5a14e5dd7587488f2498ef30d3224ea28
zabronax <zabronax@gmail.com>
2025-04-01 14:57:17 +0200

    apply migrations

--- services/QuoteService/Program.cs
+++ services/QuoteService/Program.cs
@@ -3,14 +3,26 @@ using QuoteService.Data;
 using QuoteService.Models;
 
 var envConnectionStringKey = "DB_CONNECTION_STRING";
-var connectionString = Environment.GetEnvironmentVariable(envConnectionStringKey) ??
-  throw new ApplicationException($"Environment Variable undefined! Key: {envConnectionStringKey}");
+var connectionString = Environment.GetEnvironmentVariable(envConnectionStringKey);
+if (connectionString == null)
+{
+  Console.WriteLine($"Environment Variable undefined! Using empty string. Key: {envConnectionStringKey}");
+  connectionString = "";
+}
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(connectionString));
 
 var app = builder.Build();
+
+// Apply migration on each startup. Only for local development!
+// More controlled application is required in production
+using (var scope = app.Services.CreateScope())
+{
+  var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+  dbContext.Database.Migrate();
+}
 
 app.MapGet("/quotes", async (AppDbContext context) =>
 {
