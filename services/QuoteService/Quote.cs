fa5a01b7cd1dcf62f9fa7829d6f91974f18e97cc
zabronax <zabronax@gmail.com>
2025-04-01 20:38:54 +0200

    add constraint on Quote model

--- services/QuoteService/Quote.cs
+++ services/QuoteService/Quote.cs
@@ -3,6 +3,15 @@ namespace QuoteService.Models;
 public class Quote
 {
   public int Id { get; set; }
-  public required string Content { get; set; }
-  public required string Author { get; set; } = "Anonymous";
+  public string Content { get; init; }
+  public string Author { get; init; }
+
+  public Quote(string content, string? author = null)
+  {
+    if (string.IsNullOrWhiteSpace(content))
+      throw new ArgumentException("Content cannot be empty.", nameof(content));
+
+    Content = content;
+    Author = string.IsNullOrWhiteSpace(author) ? "Anonymous" : author;
+  }
 }
